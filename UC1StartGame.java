5ba8ed3e303da976e27d8ef02d2ac5f35f9ed6db
CoderArsh <arshjeetb08@gmail.com>
2021-09-27 20:11:45 +0530

    Added: UC1 Starting Point

---
+++ UC1StartGame.java
@@ -0,0 +1,8 @@
+package SnakeNLadder;
+
+public class UC1StartGame {
+	public static void main(String[] args) {
+		int starting_point = 0;
+		System.out.println("Single Player Starting Point is : " + starting_point);
+		}
+}
